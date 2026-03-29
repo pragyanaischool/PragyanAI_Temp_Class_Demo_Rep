d6967e7573fe24c2e2927996304d785a774339c0
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-29 12:43:00 +0530

    Create Basic_Analytics.py

---
+++ Basic_Analytics.py
@@ -0,0 +1,25 @@
+import streamlit as st # for Streamlit UI
+import pandas as pd # Pandas - Data Processing
+import io # Buffer and others
+import matplotlib.pyplot as plt # Creating and Displaying Plots - Charts - Fig
+import seaborn as sns # Creating and Displaying Plots - Charts - Fig
+# -----------------------------
+# Page Config
+# -----------------------------
+st.set_page_config(page_title="PragyanAI Student Program Pricing Analytics Dashboard", layout="wide")
+# -----------------------------
+# Title
+# -----------------------------
+st.title("PragyanAI Student Program Pricing & Scholarship Analytics Dashboard")
+
+st.write("Analyze pricing, discounts, and student conversion behavior.")
+
+# -----------------------------
+# Load Data
+# -----------------------------
+@st.cache_data
+def load_data():
+    df = pd.read_csv("data/student_PRICING_SCHOLARSHIP_Analysis_Project_12.csv")
+    return df
+
+df = load_data()
